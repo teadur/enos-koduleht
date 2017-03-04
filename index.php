4cf45f92b279803a1f2fb575abf88c516e57b5e4
Georg Kahest <georg.kahest@itcollege.ee>
2017-03-04 13:49:07 +0200

    add javascript.js

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 <body>
 <h1>Vinge koduleht</h1>
 <?php echo "hello world"; ?>
+<script src="javascript.js"></script>
 
-<img src="vintage_web_tech.png" alt="Picture ITK timetable not working in Edge Browser"></img>
+<img src="vintage_web_tech.png" alt="Picture ITK timetable not working in Edge Browser" height="400" width="400"></img>
 </body>
 
 </html> 
---
+++ javascript.js
@@ -0,0 +1,2 @@
+	  //JavaScript code goes here
+	  alert('Hello World!');
