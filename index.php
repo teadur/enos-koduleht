76d82b62f85e836f4a871e733df740e5d4358f8c
Georg Kahest <georg.kahest@itcollege.ee>
2017-03-04 13:38:06 +0200

    make mysql connection, use config.php what is gitignored, provide example config.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
 <h1>Vinge koduleht</h1>
 <?php echo "hello world"; ?>
+
 <img src="vintage_web_tech.png" alt="Picture ITK timetable not working in Edge Browser"></img>
 </body>
 
---
+++ mysql.php
@@ -0,0 +1,9 @@
+<?php 
+require "config.php";
+
+    $l = mysqli_connect($host, $user, $pass, $db);
+    mysqli_query($l, "SET CHARACTER SET UTF8") or
+            die("Error, ei saa andmebaasi charsetti seatud");
+    mysqli_close($l);
+?>
+
