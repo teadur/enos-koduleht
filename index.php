ecf8c8b9fc045f0e2469a22b82f8267f77ab8da3
Georg Kahest <georg.kahest@itcollege.ee>
2017-03-04 13:23:41 +0200

    move to php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"> 
+<title>Minu vägev koduleht</title>
+</head>
+
+<body>
+<h1>Vinge koduleht</h1>
+<?php echo "hello world"; ?>
+<img src="vintage_web_tech.png" alt="Picture ITK timetable not working in Edge Browser"></img>
+</body>
+
+</html> 
